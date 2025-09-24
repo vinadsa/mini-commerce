7d35aebe8eee05cad1953ae7985c6828b51bbeba
Khoerunissa99 <Khoerunissa12nis@gmail.com>
2025-09-24 21:09:10 +0700

    nisa Add orders seeder with sample data for testing

--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -12,6 +12,53 @@ class OrdersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+            $orders = [
+                [
+                    'order_number' => 'ORD-20250924001',
+                    'user_id' => 1,
+                    'status_id' => 1,
+                    'total' => 250000.00,
+                    'total_items' => 3,
+                    'shipping_cost' => 15000.00,
+                    'tax' => 2500.00,
+                    'discount' => 5000.00,
+                    'payment_method' => 'Transfer Bank',
+                    'shipping_address_text' => 'Jl. Mawar No. 1, Jakarta',
+                    'billing_address_text' => 'Jl. Melati No. 2, Jakarta',
+                    'note' => 'Harap dikirim secepatnya',
+                ],
+                [
+                    'order_number' => 'ORD-20250924002',
+                    'user_id' => 2,
+                    'status_id' => 2,
+                    'total' => 120000.00,
+                    'total_items' => 1,
+                    'shipping_cost' => 10000.00,
+                    'tax' => 1200.00,
+                    'discount' => 0.00,
+                    'payment_method' => 'COD',
+                    'shipping_address_text' => 'Jl. Kenanga No. 3, Bandung',
+                    'billing_address_text' => 'Jl. Kenanga No. 3, Bandung',
+                    'note' => null,
+                ],
+                [
+                    'order_number' => 'ORD-20250924003',
+                    'user_id' => 1,
+                    'status_id' => 3,
+                    'total' => 500000.00,
+                    'total_items' => 5,
+                    'shipping_cost' => 20000.00,
+                    'tax' => 5000.00,
+                    'discount' => 10000.00,
+                    'payment_method' => 'E-Wallet',
+                    'shipping_address_text' => 'Jl. Anggrek No. 4, Surabaya',
+                    'billing_address_text' => 'Jl. Anggrek No. 4, Surabaya',
+                    'note' => 'Packing aman ya',
+                ],
+            ];
+
+            foreach ($orders as $order) {
+                \DB::table('orders')->insert($order);
+            }
     }
 }
