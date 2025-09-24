8c74c0cbfaca5f500fabf2fe2f580a3fc9d801a6
Khoerunissa99 <Khoerunissa12nis@gmail.com>
2025-09-24 21:03:22 +0700

    nisa Add seeders for order statuses and product images

--- database/seeders/OrderStatusSeeder.php
+++ database/seeders/OrderStatusSeeder.php
@@ -12,6 +12,18 @@ class OrderStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+            $statuses = [
+                ['name' => 'Menunggu Pembayaran', 'sort_order' => 1],
+                ['name' => 'Sudah Dibayar', 'sort_order' => 2],
+                ['name' => 'Sedang Diproses', 'sort_order' => 3],
+                ['name' => 'Sedang Dikirim', 'sort_order' => 4],
+                ['name' => 'Sudah Diterima', 'sort_order' => 5],
+                ['name' => 'Dibatalkan', 'sort_order' => 6],
+                ['name' => 'Dana Dikembalikan', 'sort_order' => 7],
+            ];
+
+            foreach ($statuses as $status) {
+                \DB::table('order_statuses')->insert($status);
+            }
     }
 }
--- database/seeders/ProductImagesSeeder.php
+++ database/seeders/ProductImagesSeeder.php
@@ -12,6 +12,35 @@ class ProductImagesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $images = [
+            [
+                'product_id' => 1,
+                'url' => 'https://example.com/images/product1-1.jpg',
+                'is_primary' => true,
+                'sort_order' => 1,
+            ],
+            [
+                'product_id' => 1,
+                'url' => 'https://example.com/images/product1-2.jpg',
+                'is_primary' => false,
+                'sort_order' => 2,
+            ],
+            [
+                'product_id' => 2,
+                'url' => 'https://example.com/images/product2-1.jpg',
+                'is_primary' => true,
+                'sort_order' => 1,
+            ],
+            [
+                'product_id' => 3,
+                'url' => 'https://example.com/images/product3-1.jpg',
+                'is_primary' => true,
+                'sort_order' => 1,
+            ],
+        ];
+
+        foreach ($images as $image) {
+            \DB::table('product_images')->insert($image);
+        }
     }
 }
