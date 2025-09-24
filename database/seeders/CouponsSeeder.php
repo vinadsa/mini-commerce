b7958b09c8efa21b3af89d5084e8ec7bb0cdc16d
Khoerunissa99 <Khoerunissa12nis@gmail.com>
2025-09-24 22:03:54 +0700

    nisa Add CouponsSeeder to populate coupon data in the database

--- database/seeders/CouponsSeeder.php
+++ database/seeders/CouponsSeeder.php
@@ -12,6 +12,44 @@ class CouponsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+            $coupons = [
+                [
+                    'code' => 'DISC10',
+                    'type' => 'percentage',
+                    'value' => 10.00,
+                    'min_order_amount' => 100000.00,
+                    'expires_at' => now()->addDays(30),
+                    'usage_limit' => 100,
+                    'used_count' => 1,
+                    'is_active' => true,
+                    'created_at' => now()->subDays(5),
+                ],
+                [
+                    'code' => 'ONGKIR15K',
+                    'type' => 'fixed',
+                    'value' => 15000.00,
+                    'min_order_amount' => 200000.00,
+                    'expires_at' => now()->addDays(15),
+                    'usage_limit' => 50,
+                    'used_count' => 0,
+                    'is_active' => true,
+                    'created_at' => now()->subDays(3),
+                ],
+                [
+                    'code' => 'EWALLET20',
+                    'type' => 'percentage',
+                    'value' => 20.00,
+                    'min_order_amount' => 300000.00,
+                    'expires_at' => now()->addDays(10),
+                    'usage_limit' => 20,
+                    'used_count' => 1,
+                    'is_active' => true,
+                    'created_at' => now()->subDays(1),
+                ],
+            ];
+
+            foreach ($coupons as $coupon) {
+                \DB::table('coupons')->insert($coupon);
+            }
     }
 }
